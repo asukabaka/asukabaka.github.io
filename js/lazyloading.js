439b0624f5477d6ca034095cc45651b200d7152b
asukabaka <48495833+asukabaka@users.noreply.github.com>
2019-09-28 19:59:16 -0700

    Update lazyloading.js

--- js/lazyloading.js
+++ js/lazyloading.js
@@ -1,5 +1,11 @@
 // GOOGLE WEB FUNDEMENTALS BY JEREMY WAGNER
 // https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video
+//Wagner's Notes --------------------------------------------------------------------------------------------------------------------------------
+//This code uses getBoundingClientRect in a scroll event handler to check if any of img.lazy elements are in the viewport. 
+//A setTimeout call is used to delay processing, and an active variable contains the processing state which is used to throttle function calls. 
+//As images are lazy loaded, they're removed from the elements array. 
+//When the elements array reaches a length of 0, the scroll event handler code is removed. 
+//-----------------------------------------------------------------------------------------------------------------------------------------------
 
 //Ray's notes on this code
 //Lazy loading is when images are only loaded as the user scrolls down the page. This is often seen in websites that infinitely scroll down because it saves unnesscary
@@ -12,13 +18,16 @@ document.addEventListener("DOMContentLoaded", function() {
   //"The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, 
   //without waiting for stylesheets, images, and subframes to finish loading."-MOZILLA MDN
   let lazyImages = [].slice.call(document.querySelectorAll("img.cyberpunk_hifumi_resize"));
+  //This code specifically targets all elements that are img.cyberpunk_hifumi_resize
   let active = false;
 
   const lazyLoad = function() {
     if (active === false) {
+      //=== means strict equality in both value and type.
       active = true;
 
       setTimeout(function() {
+      //setTimeout means the script will run every 200milliseconds to see if the user is scrolling or viewport has changed.
         lazyImages.forEach(function(lazyImage) {
           if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
             lazyImage.src = lazyImage.dataset.src;
@@ -33,6 +42,9 @@ document.addEventListener("DOMContentLoaded", function() {
               document.removeEventListener("scroll", lazyLoad);
               window.removeEventListener("resize", lazyLoad);
               window.removeEventListener("orientationchange", lazyLoad);
+              //These 3 event listeners are for scrolling, resizing the viewport, or changing orientation from landscape to portrait or vice versa.
+              //I THINK this means when you have lazy loaded all images it "turns off" the code from running to save processing power?
+              //If there are no listeners there is nothing for setTimeout to do.
             }
           }
         });
@@ -45,4 +57,5 @@ document.addEventListener("DOMContentLoaded", function() {
   document.addEventListener("scroll", lazyLoad);
   window.addEventListener("resize", lazyLoad);
   window.addEventListener("orientationchange", lazyLoad);
+  //These 3 event listeners are for scrolling, resizing the viewport, or changing orientation from landscape to portrait or vice versa.
 });
