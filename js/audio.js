ab041ad40e649ce2dc5433e5ed4197d5acc66ae1
asukabaka <48495833+asukabaka@users.noreply.github.com>
2019-11-18 08:55:53 -0800

    sound

---
+++ js/audio.js
@@ -0,0 +1,6 @@
+
+var test = document.getElementById("side1");
+test.addEventListener("mouseover", function( event ) {   
+    var audio = document.getElementById("audio");
+    audio.play();
+}, false);
