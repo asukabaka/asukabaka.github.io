479d5c195f9d3fbd8fbbff9d69e5fb6f632c93c9
Raymond Lam <qu4ntumphysx@gmail.com>
2021-04-06 04:31:29 -0700

    instafeed

---
+++ instafeed-tutorial.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+    var feed = new Instafeed({
+      accessToken: 'IGQVJYUkZAjZAnBwa2NSaDNDbGE0MzFWakdjYVVqQm8tRmI2SndpblRYU2FHU3A1U3d0d2NNNXR6eFZA0RGtORzh1YXBhZAzhNbXgwRFNKVGdWSU9wWDFMNG9qUmczZAHZAQaDVhYmFUY0EtMTBpWnVTLUpZAcG4zTno0cmRnVWE0'
+    });
+    feed.run();
+</script>
