59d027c12400c5bf6c545afd178315bf9f93b119
Raymond Lam <qu4ntumphysx@gmail.com>
2021-05-14 18:41:53 -0700

    swup

---
+++ js/swup-init.js
@@ -0,0 +1,15 @@
+import SwupProgressPlugin from 'SwupProgressPlugin.js';
+
+const options = {
+  className: 'swup-progress-bar',
+  transition: 300,
+  delay: 300
+}
+
+
+const swup = new Swup(
+{
+  plugins: [new SwupProgressPlugin(options)]
+
+}
+); // only this line when included with script tag
